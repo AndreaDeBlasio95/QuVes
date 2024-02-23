2580f488f8314f80d5735662b0e326f3cf6c1df6
AndreaDeBlasio95 <sviluppatoredeblasioandrea@gmail.com>
2024-02-23 13:07:59 +0100

    improvements on movement

--- Assets/Scripts/MainCharacterController.cs
+++ Assets/Scripts/MainCharacterController.cs
@@ -30,19 +30,40 @@ public class MainCharacterController : MonoBehaviour
     private Vector3 _vel;
 
     [SerializeField]
+    private int _countJump = 0; // to allow multiple jumps
+
+    [SerializeField]
+    private int _maxJumps = 2; // to allow multiple jumps
+
+    [SerializeField]
+    private bool _isOnAir = false;
+
+    [SerializeField]
     private float _jumpForce = 5f;
 
     [SerializeField]
-    private float _initSpeed = 15f;
+    private Vector3 _customGravity;
 
     [SerializeField]
-    private float _currentSpeed = 15f;
+    private float _jumpForceLimitController = 5f;
+
+    [SerializeField]
+    private float _jumpForceLimitControllerTimer = 0.5f;
+
+    [SerializeField]
+    private float _onAirSpeed = 4f;
+
+    [SerializeField]
+    private float _initSpeed = 7f;
+
+    [SerializeField]
+    private float _currentSpeed = 7f;
 
     [SerializeField]
     private float _acceleration = 5f;
 
     [SerializeField]
-    private float _maxSpeed = 30f;
+    private float _maxSpeed = 20f;
 
     // CUSTOM MOVEMENTS
     [Header("CUSTOM MOVEMENTS")]
@@ -52,7 +73,6 @@ public class MainCharacterController : MonoBehaviour
 
     // ANIMATIONS
     [Header("ANIMATIONS")]
-
     [SerializeField]
     private bool _isRunning;
 
@@ -104,6 +124,8 @@ public class MainCharacterController : MonoBehaviour
         _hook = false;
         _isHooked = false;
 
+        _countJump = 0;
+
         aimTargetObj.SetActive(false);
 
         _defaultPlayerActions = new DefaultPlayerActions();
@@ -138,6 +160,8 @@ public class MainCharacterController : MonoBehaviour
         moveCamera.SetActive(false);
         aimCamera.SetActive(true);
         aimTargetObj.SetActive(true);
+        _animator.SetBool("isWallRunning", false);
+        _animator.SetBool("isRunning", false);
 
     }
     private void NotAim()
@@ -184,25 +208,32 @@ public class MainCharacterController : MonoBehaviour
 
     private void OnJump(InputAction.CallbackContext context)
     {
-        _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-        ResetPhysics();
+        if (_countJump < _maxJumps)
+        {
+            ResetPhysics();
+            _countJump++;
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+
+            _isOnAir = true;
+            StartCoroutine(JumpLimitController());
+        }
     }
 
-    private void Dump ()
+    private void Dump()
     {
         _dump = true;
         _rigidbody.AddForce(Vector3.down * _jumpForce * 1.1f, ForceMode.Impulse);
     }
     private void DumpFallBack()
     {
-        _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        _rigidbody.AddForce(Vector3.up * _jumpForce * 0.2f, ForceMode.Impulse);
     }
-    private void ResetPhysics ()
+    private void ResetPhysics()
     {
         _rigidbody.useGravity = true;
         _hook = false;
         _isHooked = false;
-        _currentSpeed = _initSpeed;
+        _animator.SetBool("isWallRunning", false);
     }
     private void FixedUpdate()
     {
@@ -211,24 +242,37 @@ public class MainCharacterController : MonoBehaviour
         {
             if (!_isHooked)
             {
-                
+
                 if (!_hook)
                 {
                     #region Player Movements on the Ground 
                     _moveDir = _moveAction.ReadValue<Vector2>();
 
                     // Gradually increase the speed until it reaches the maxSpeed.
-                    if (_currentSpeed < _maxSpeed && (_moveDir.x != 0 || _moveDir.y != 0))
+                    if (!_isOnAir)
                     {
-                        _currentSpeed += _acceleration * Time.fixedDeltaTime;
-                        _currentSpeed = Mathf.Min(_currentSpeed, _maxSpeed);
+                        if (_currentSpeed < _maxSpeed && (_moveDir.x != 0 || _moveDir.y != 0))
+                        {
+                            _currentSpeed += _acceleration * Time.fixedDeltaTime;
+                            _currentSpeed = Mathf.Min(_currentSpeed, _maxSpeed);
+                        }
+
+                    } else
+                    {
+                        // movement Speed
+                        if (_currentSpeed > _initSpeed)
+                        {
+                            _currentSpeed -= _acceleration * Time.fixedDeltaTime;
+                        }
+
+                        // Rigidbody
+                        _rigidbody.AddForce(_customGravity * _rigidbody.mass, ForceMode.Acceleration);
                     }
 
                     _vel = _rigidbody.velocity;
                     _vel.x = _currentSpeed * _moveDir.x;
                     _vel.z = _currentSpeed * _moveDir.y;
                     _rigidbody.velocity = _vel;
-
 
                     // --- ROTATION ---
                     //Vector2 lookDir = _lookAction.ReadValue<Vector2>();
@@ -243,7 +287,7 @@ public class MainCharacterController : MonoBehaviour
                     }
 
                     // --- ANIMATION ---
-                    if (!_isRunning && (_vel.x != 0 || _vel.z != 0) && !_aim)
+                    if (!_isRunning && (_vel.x != 0 || _vel.z != 0) && !_aim && !_isOnAir)
                     {
                         _isRunning = true;
                         _animator.SetBool("isRunning", true);
@@ -252,7 +296,6 @@ public class MainCharacterController : MonoBehaviour
                     {
                         _isRunning = false;
                         _animator.SetBool("isRunning", false);
-                        _currentSpeed = _initSpeed;
                     }
                     // --- 
                     #endregion Player Movements ends -
@@ -267,12 +310,12 @@ public class MainCharacterController : MonoBehaviour
                                                                      // Gradually increase the speed until it reaches the maxSpeed.
                         if (_currentSpeed < _hookMaxSpeed)
                         {
-                            _currentSpeed += _acceleration * 2 * Time.fixedDeltaTime;
+                            _currentSpeed += _acceleration * Time.fixedDeltaTime;
                             _currentSpeed = Mathf.Min(_currentSpeed, _hookMaxSpeed);
                         }
                         _rigidbody.useGravity = false;
                         transform.position = Vector3.MoveTowards(transform.position, (Vector3)_hookTargetPosition, step);
-                        
+
                     }
                     #endregion Hooking system ends -
                 }
@@ -282,7 +325,8 @@ public class MainCharacterController : MonoBehaviour
                 float input = Input.GetAxis("Vertical");
 
                 Vector3 moveDirection = hookController.HookedObject().transform.forward * input; // Use the wall's forward direction for movement
-                transform.position += moveDirection * _currentSpeed * Time.deltaTime;
+                // todo: change 10 with a wall running speed
+                transform.position += moveDirection * 10 * Time.deltaTime;
 
                 // Rotate the player to face the direction of movement
                 if (moveDirection != Vector3.zero)
@@ -304,7 +348,6 @@ public class MainCharacterController : MonoBehaviour
                     {
                         _isRunning = false;
                         _animator.SetBool("isWallRunning", false);
-                        _currentSpeed = _initSpeed;
                     }
                     // ---
                 }
@@ -322,32 +365,55 @@ public class MainCharacterController : MonoBehaviour
                 ResetPhysics();
                 DumpFallBack();
             }
-            ResetPhysics();
+
+            ResetJumpCount();
+            _isOnAir = false;
         }
         if (collision.gameObject.CompareTag("Wall"))
         {
             if (_isHooked == false && _hook)
             {
-                _rigidbody.velocity = new Vector3(0,0,0);
+                _rigidbody.velocity = new Vector3(0, 0, 0);
                 _hook = false;
                 _isHooked = true;
                 transform.position = (Vector3)_hookTargetPosition;
                 _currentSpeed = _initSpeed;
                 _animator.SetBool("isWallRunning", true);
+
+                ResetJumpCount();
+                _isOnAir = false;
             }
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Wall"))
+        if (other.gameObject.CompareTag("ExitWall"))
         {
             if (_isHooked == true)
             {
                 Debug.Log("Exit from Wall");
                 _currentSpeed = _initSpeed;
-                Dump();
+
+                ResetPhysics();
+                ResetJumpCount();
+
+                _isHooked = false;
+                _isOnAir = true;
             }
+
         }
 
+    }
+
+    private void ResetJumpCount()
+    {
+        _countJump = 0;
+    }
+
+    private IEnumerator JumpLimitController()
+    {
+        yield return new WaitForSeconds(_jumpForceLimitControllerTimer);
+        //_rigidbody.AddForce(Vector3.down * _jumpForceLimitController, ForceMode.Impulse);
+        
     }
 }
